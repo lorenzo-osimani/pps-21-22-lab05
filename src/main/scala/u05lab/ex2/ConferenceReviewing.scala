521d87611d7adf26badfba6ee28a12cc38081eac
Lorenzo <lorenzo.osimani@studio.unibo.it>
2022-04-04 12:26:49 +0200

    Ex 2-3

--- src/main/scala/u05lab/ex2/ConferenceReviewing.scala
+++ src/main/scala/u05lab/ex2/ConferenceReviewing.scala
@@ -1,7 +1,63 @@
 package u05lab.ex2
 
+import java.util.stream.Collectors
+
 trait ConferenceReviewing:
 
-  def loadReview(article: Int, scores: Map[Int, Int]): Unit
+  enum Question:
+    case RELEVANCE
+    case SIGNIFICANCE
+    case CONFIDENCE
+    case FINAL
 
-  def orderedScores(article: Int, relevance: Int): Unit
+  def loadReview(article: Int, scores: Map[Question, Int]): Unit
+
+  def loadReview(article: Int, relevance: Int, significance: Int, confidence: Int, fin: Int): Unit
+
+  def orderedScores(article: Int, question: Question): List[Int]
+
+  def averageFinalScore(article: Int): Double
+
+  def acceptedArticles: Set[Int]
+
+  def sortedAcceptedArticles: List[(Int, Double)]
+
+  def averageWeightedFinalScoreMap: Map[Int, Double]
+
+object ConferenceReviewing:
+  def apply(): ConferenceReviewing = new ConferenceReviewingImpl
+
+class ConferenceReviewingImpl extends ConferenceReviewing:
+
+  import scala.collection.mutable.ListBuffer
+  import Question.*
+
+  val reviews = ListBuffer[(Int, Map[Question, Int])]()
+
+  override def loadReview(article: Int, scores: Map[Question, Int]): Unit =
+    if scores.size < Question.values.length then throw IllegalArgumentException()
+    reviews += (article -> scores)
+
+  override def loadReview(article: Int, relevance: Int, significance: Int, confidence: Int, fin: Int): Unit =
+    val scores = Map((RELEVANCE -> relevance), (SIGNIFICANCE -> significance), (CONFIDENCE -> confidence), (FINAL -> fin))
+    loadReview(article, scores)
+
+  override def orderedScores(article: Int, question: Question): List[Int] =
+    reviews.filter(_._1 == article).map(_._2.get(question).get).sorted.toList
+
+  override def averageFinalScore(article: Int): Double =
+    val usefulReviews = reviews.filter(_._1 == article)
+    usefulReviews.map(_._2.get(FINAL).get).sum / usefulReviews.length
+
+  private def accepted(article: Int): Boolean =
+    averageFinalScore(article) > 5 && reviews.filter(_._1 == article).flatMap(_._2).filter(x => x._1 == RELEVANCE && x._2 > 8).nonEmpty
+
+  override def acceptedArticles: Set[Int] =
+    reviews.map(_._1).distinct.filter(accepted(_)).toSet
+
+  override def sortedAcceptedArticles: List[(Int, Double)] =
+    this.acceptedArticles.map(r => (r, this.averageFinalScore(r)))
+      .toList.sorted((x, y) => x._2.compareTo(y._2))
+
+  override def averageWeightedFinalScoreMap: Map[Int, Double] =
+    reviews.map(_._1).distinct.map(r => (r, this.averageFinalScore(r))).toMap
--- src/main/scala/u05lab/ex3/Collections.scala
+++ src/main/scala/u05lab/ex3/Collections.scala
@@ -1,7 +1,7 @@
 package u05lab.ex3
 
 import java.util.concurrent.TimeUnit
-
+import scala.collection.mutable.ListBuffer
 import scala.concurrent.duration.FiniteDuration
 
 object PerformanceUtils:
@@ -19,16 +19,107 @@ object PerformanceUtils:
 
 @main def checkPerformance: Unit =
 
+  import collection.*
+  import PerformanceUtils.*
+
+  val testValues = (1 to 10000000)
+
   /* Linear sequences: List, ListBuffer */
+  lazy val list = testValues.toList
+  lazy val lazyList: LazyList[Int] = LazyList.from(testValues)
+  lazy val listBuffer: ListBuffer[Int] = mutable.ListBuffer.from(testValues)
+  println("Linear Sequences -----------------------")
+  println("------- Create operation")
+  measure("List")(list)
+  measure("LazyList")(lazyList)
+  measure("ListBuffer")(listBuffer)
+  println("------- Size operation")
+  measure("List")(list.size)
+  measure("LazyList")(lazyList.size)
+  measure("ListBuffer")(listBuffer.size)
+  println("------- Access Tail operation")
+  measure("List")(list.tail)
+  measure("LazyList")(lazyList.tail)
+  measure("ListBuffer")(listBuffer.tail)
+  println("------- Append operation")
+  measure("List")(list :+ 0)
+  measure("LazyList")(lazyList :+ 0)
+  measure("ListBuffer")(listBuffer += 0)
+  println("------- Remove operation")
+  measure("ListBuffer")(listBuffer -= 0)
+  println("------- Update operation")
+  measure("List")(list.updated(1, 0))
+  measure("LazyList")(lazyList.updated(1, 0))
+  measure("ListBuffer")(listBuffer(1) = 0)
+  println("-----------------------------------------")
 
   /* Indexed sequences: Vector, Array, ArrayBuffer */
+  lazy val vector = testValues.toVector
+  lazy val array = testValues.toArray
+  lazy val arrayBuffer = mutable.ArrayBuffer.from(testValues)
+  println("\nIndexed Sequences -----------------------")
+  println("------- Create operation")
+  measure("Vector")(vector)
+  measure("Array")(array)
+  measure("ArrayBuffer")(arrayBuffer)
+  println("------- Size operation")
+  measure("Vector")(vector.size)
+  measure("Array")(array.size)
+  measure("ArrayBuffer")(arrayBuffer.size)
+  println("------- Access Tail operation")
+  measure("Vector")(vector.tail)
+  measure("Array")(array.tail)
+  measure("ArrayBuffer")(arrayBuffer.tail)
+  println("------- Append operation")
+  measure("Vector")(vector :+ 0)
+  measure("Array")(array  :+ 0)
+  measure("ArrayBuffer")(arrayBuffer  += 0)
+  println("------- Remove operation")
+  measure("ArrayBuffer")(arrayBuffer -= 0)
+  println("------- Update operation")
+  measure("Vector")(vector.updated(1, 0))
+  measure("Array")(array.updated(1, 0))
+  measure("ArrayBuffer")(arrayBuffer(1) = 0)
+  println("-----------------------------------------")
 
   /* Sets */
+  lazy val set = testValues.toSet
+  lazy val mutableSet = mutable.Set.from(testValues)
+  println("\nSets -----------------------")
+  println("------- Create operation")
+  measure("Set")(set)
+  measure("MutableSet")(mutableSet)
+  println("------- Size operation")
+  measure("Set")(set.size)
+  measure("MutableSet")(mutableSet.size)
+  println("------- Access Tail operation")
+  measure("Set")(set.tail)
+  measure("MutableSet")(mutableSet.tail)
+  println("------- Append operation")
+  measure("MutableSet")(set + 0)
+  measure("MutableSet")(mutableSet += 0)
+  println("------- Remove operation")
+  measure("MutableSet")(set - 1)
+  measure("MutableSet")(mutableSet -= 0)
+  println("-----------------------------------------")
 
   /* Maps */
-
-  /* Comparison */
-  import PerformanceUtils.*
-  val lst = (1 to 10000000).toList
-  val vec = (1 to 10000000).toVector
-  assert(measure("lst last")(lst.last) > measure("vec last")(vec.last))
+  lazy val map = testValues.map(x => (x, x)).toMap
+  lazy val mutableMap: mutable.Map[Int, Int] = mutable.Map.from(testValues.map(x => (x, x)))
+  println("\nMaps -----------------------")
+  println("------- Create operation")
+  measure("Map")(map)
+  measure("MutableMap")(mutableMap)
+  println("------- Size operation")
+  measure("Map")(map.size)
+  measure("MutableMap")(mutableMap.size)
+  println("------- Access operation")
+  measure("Map")(map(100))
+  measure("MutableMap")(mutableMap(100))
+  println("------- Append operation")
+  measure("Map")(map + (0 -> 0))
+  measure("MutableMap")(mutableMap += (0 -> 0))
+  println("------- Remove operation")
+  measure("Map")(map - 1)
+  measure("MutableMap")(mutableMap -= 0)
+  println("-----------------------------------------")
